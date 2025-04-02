a4567300344cbe2c9391ca27a40196848931b972
Andre Fernandes <andre.fernandesdev@gmail.com>
2025-04-02 17:59:24 +0100

    fix title on homepage

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -3,10 +3,12 @@ import ListFilter from '@/components/list-filter';
 import PostList from '@/components/post-list';
 import MainLayout from '@/layouts/main-layout';
 import { Post, Tag } from '@/types';
+import { Head } from '@inertiajs/react';
 
 export default function Welcome({ posts, tags }: { posts: { data: Post[] }; tags: { data: Tag[] } }) {
     return (
         <MainLayout bannerVariant="lg">
+            <Head title="Welcome" />
             <ListFilter
                 tags={tags.data}
                 type="post"
